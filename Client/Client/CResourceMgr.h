cbfb420c6bb944255b6056afaed687be9eb65563
HyunjungWoo <112833211+HyunjungWoo@users.noreply.github.com>
2023-11-29 12:06:23 +0900

    [클라] SoundMgr 생성, Sound생성, CResource코드 수정

--- Client/Client/CResourceMgr.h
+++ Client/Client/CResourceMgr.h
@@ -1,16 +1,18 @@
 #pragma once
 
 class CTexture;
-
+class CSound;
 class CResourceMgr
 {
 	SINGLE(CResourceMgr)
 private:
 	std::map<std::wstring, CTexture*>	m_mapTex;
+	std::map<std::wstring, CSound*>		m_mapSound;
 
 public:
 	CTexture* LoadTexture(const std::wstring& _strKey, const std::wstring& _strRelativePath);
 	CTexture* FindTexture(const std::wstring& _strKey);
 
+	CSound* LoadSound(const std::wstring& _strKey, const std::wstring& _strRelativePath);
+	CSound* FindSound(const std::wstring& _strkey);
 };
-
